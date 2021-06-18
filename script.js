849e341b1d79fc8e035ddf3f66249f3ae3db258a
TheAkshayCoder <thakurakshay8375@gmail.com>
2021-06-18 13:41:06 +0530

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const container = document.getElementById('container')
 var id = window.location.href;
-var fullId = id.substring(id.indexOf("=", 60) + 1, id.indexOf("ok", 60));
+// var fullId = id.substring(id.indexOf("=", 60) + 1, id.indexOf("ok", 60));
 console.log(container)
-// var fullId = 28
+var fullId = 28
 const newI= document.createElement('iframe')
 newI.setAttribute('src',`https://besingularlms.herokuapp.com/getStudentRecord/${fullId}`)
+container.appendChild(newI)
 
 
-    container.appendChild(newI)
+async function convertPdf(){
+    const response = await fetch(`https://besingularlms.herokuapp.com/convertPdf/${fullId}`)
+    console.log(response)
+
+    if (response.status==200){
+        console.log(`https://besingularlms.herokuapp.com/downloadReport/${fullId}`)
+    }
+}
