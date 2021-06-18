2e3d265657fcc7d8d4983561a711332d29ce8f49
TheAkshayCoder <thakurakshay8375@gmail.com>
2021-06-18 11:30:10 +0530

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.getElementById('container')
 var id = window.location.href;
 var fullId = id.substring(id.indexOf("=", 60) + 1, id.indexOf("ok", 60));
+console.log(container)
+// var fullId = 28
+const newI= document.createElement('iframe')
+newI.setAttribute('src',`https://besingularlms.herokuapp.com/getStudentRecord/${fullId}`)
 
 
-container.innerHTML=`<iframe src="https://besingularlms.herokuapp.com/getStudentRecord/${fullId}" frameborder="0"></iframe>`
+    container.appendChild(newI)
