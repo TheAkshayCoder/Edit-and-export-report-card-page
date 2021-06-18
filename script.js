1cad66ccdfe7d8265eb5eb5abdf9d22bbf6d97db
TheAkshayCoder <thakurakshay8375@gmail.com>
2021-06-18 10:22:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.getElementById('container')
+
+
+
+container.innerHTML=`<iframe src="https://besingularlms.herokuapp.com/getStudentRecord/${id}" frameborder="0"></iframe>`
