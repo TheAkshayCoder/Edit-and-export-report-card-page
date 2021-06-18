62f7246f6016be52c4e1ba32e4c82d3b5a21e406
TheAkshayCoder <thakurakshay8375@gmail.com>
2021-06-18 17:45:08 +0530

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 btn.classList.add('active')
 download.classList.remove('active')
 
@@ -5,13 +6,14 @@ const container = document.getElementById('container')
 var id = window.location.href;
 var fullId = id.substring(id.indexOf("=", 60) + 1, id.indexOf("ok", 60));
 console.log(container)
-// var fullId = 28
+
 const newI= document.createElement('iframe')
 newI.setAttribute('src',`https://besingularlms.herokuapp.com/getStudentRecord/${fullId}`)
 container.appendChild(newI)
 
 
 async function convertPdf(){
+
     document.getElementById('convertPdf').disabled=true;
     const response = await fetch(`https://besingularlms.herokuapp.com/convertPdf/${fullId}`)
     console.log(response)
@@ -24,5 +26,10 @@ async function convertPdf(){
         download.classList.add('active')
         download.setAttribute('href',`https://besingularlms.herokuapp.com/downloadReport/${fullId}`)
         download.setAttribute('target','_blank')
+    } else if (response.status==404){
+        newI.style.display='none'
+        const errDiv= document.createElement('div')
+        errDiv.innerHTML=`<h1>Sorry, Report card not found</h1>`
+        container.appendChild(errDiv)
     }
 }
