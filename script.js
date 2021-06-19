11a0eb66f9017787041713df98069cf6339d4840
TheAkshayCoder <thakurakshay8375@gmail.com>
2021-06-19 11:03:24 +0530

    final commit

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ container.appendChild(newI)
 
 async function convertPdf(){
 
+    document.getElementById('loader').style.display='block'
+
     document.getElementById('convertPdf').disabled=true;
     const response = await fetch(`https://besingularlms.herokuapp.com/convertPdf/${fullId}`)
     response
 
     if (response.status==200){
+        document.getElementById('loader').style.display='none'
         console.log(`https://besingularlms.herokuapp.com/downloadReport/${fullId}`)
         let download = document.getElementById('download')
         let btn = document.getElementById('btn')
@@ -26,6 +29,7 @@ async function convertPdf(){
         download.setAttribute('href',`https://besingularlms.herokuapp.com/downloadReport/${fullId}`)
         download.setAttribute('target','_blank')
     } else if (response.status==404){
+        document.getElementById('loader').style.display='none'
         newI.style.display='none'
         const errDiv= document.createElement('div')
         errDiv.innerHTML=`<h1>Sorry, Report card not found</h1>`
