e2e9a03d3a8b9c7818b27317ebf1fb43bfe83ea6
TheAkshayCoder <thakurakshay8375@gmail.com>
2021-06-19 09:10:09 +0530

    not final

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
-btn.classList.add('active')
-download.classList.remove('active')
+
 
 const container = document.getElementById('container')
 var id = window.location.href;
 var fullId = id.substring(id.indexOf("=", 60) + 1, id.indexOf("ok", 60));
-console.log(container)
-
+// console.log(container)
+// var fullId = 28
 const newI= document.createElement('iframe')
 newI.setAttribute('src',`https://besingularlms.herokuapp.com/getStudentRecord/${fullId}`)
 container.appendChild(newI)
@@ -31,5 +30,22 @@ async function convertPdf(){
         const errDiv= document.createElement('div')
         errDiv.innerHTML=`<h1>Sorry, Report card not found</h1>`
         container.appendChild(errDiv)
+    } else{
+            
+            let download = document.getElementById('download')
+            let btn = document.getElementById('btn')
+            btn.classList.remove('active')
+            download.classList.add('active')
+            download.setAttribute('href',`https://besingularlms.herokuapp.com/downloadReport/${fullId}`)
+            download.setAttribute('target','_blank')
+        
     }
+
 }
+
+let download = document.getElementById('download')
+var btn = document.getElementById('btn')
+btn.classList.remove('active')
+download.classList.add('active')
+download.setAttribute('href',`https://besingularlms.herokuapp.com/downloadReport/${fullId}`)
+download.setAttribute('target','_blank')
